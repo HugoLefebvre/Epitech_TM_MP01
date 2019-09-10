4af018bf4d43b8d4e388f60eaf77bdad2a4cd389
Mineyuki <mchou@hotmail.fr>
2019-09-10 15:42:13 +0200

    Modify routes

--- api/lib/api_web/router.ex
+++ api/lib/api_web/router.ex
@@ -13,22 +13,44 @@ defmodule ApiWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", ApiWeb do
-    pipe_through :browser # Use the default browser stack
+#  scope "/", ApiWeb do
+#    pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
-  end
+#    get "/", PageController, :index
+#  end
 
   scope "/api", MainWeb do
     pipe_through :api # Use the default browser stack
 
-    get "/", PageController, :index
-		resources "/users", UserController, except: [:new, :edit]
-		resources "/workingtimes", WorkingTimeController do
-			get "/users", UserController, except: [:new, :edit] 
-		end
-		resources "/clocking", ClockingController, except: [:new, :edit, :index, :update, :delete]
+#    get "/", PageController, :index
   end
+
+	scope "/api/users" do
+		pipe_through :api
+	
+		get("/", UserController, :show)
+		get "/:userID", UserController, :show
+		post "/", UserController, :create
+		put "/:userID", UserController, :update
+		delete "/:userID", UserController, :delete
+	end
+
+	scope "/api/workingtimes" do
+		pipe_through :api
+
+		get("/:userID", WorkingTimeController, :index)
+		get "/:userID/:workingtimeID", WorkingTimeController, :show
+		post "/:userID", WorkingTimeController, :create
+		put "/:id", WorkingTimeController, :update
+		delete "/:id", WorkingTimeController, :delete
+	end
+
+	scope "/api/clocks" do
+		pipe_through :api
+	
+		get "/:userID", ClockingController, :show
+		post "/:userID", ClockingController, :create
+	end
 
   # Other scopes may use custom stacks.
   # scope "/api", ApiWeb do
