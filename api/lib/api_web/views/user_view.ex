b23ee32866bd54c0020bc63382287666af33f34f
Mineyuki <mchou@hotmail.fr>
2019-09-27 14:19:05 +0200

    Change name token sign_in in view user

--- api/lib/api_web/views/user_view.ex
+++ api/lib/api_web/views/user_view.ex
@@ -8,22 +8,8 @@ defmodule ApiWeb.UserView do
     %{data: render_many(users, UserView, "user.json")}
   end
 
-  def render("indexCurrentUser.json",  %{users: users, currentUser: currentUser}) do
-    %{
-        data: render_many(users, UserView, "user.json"),
-        currentUser: render_one(currentUser, UserView, "minimalUser.json")
-      }
-  end
-
   def render("show.json", %{user: user}) do
     %{data: render_one(user, UserView, "user.json")}
-  end
-
-  def render("showCurrentUser.json", %{user: user, currentUser: currentUser}) do 
-    %{
-        data: render_one(user, UserView, "user.json"),
-        currentUser: render_one(currentUser, UserView, "minimalUser.json")
-      }
   end
 
   def render("user.json", %{user: user}) do
@@ -33,18 +19,13 @@ defmodule ApiWeb.UserView do
       password: user.password_hash}
   end
 
-  def render("minimalUser.json", %{user: user}) do 
-    %{id: user.id,
-      role: user.role_id}
-  end
-
   def render("jwt.json", %{jwt: jwt}) do
     %{jwt: jwt}
   end
 
   def render("sign_in.json", %{jwt: token, claims: claims}) do 
     %{
-      token: token,
+      jwt: token,
       idCurrentUser: claims["id"],
       roleCurrentUser: claims["role"]
     }
